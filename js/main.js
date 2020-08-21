9e01c62203847966bd0a95e5ef8752e509c5437c
Ememekong001 <airmyekong@gmail.com>
2020-08-21 15:49:37 +0100

    Added files

---
+++ js/main.js
@@ -0,0 +1,72 @@
+<section class="lays">
+      <div class="container-fluid overt ">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 mt-5">
+              <img src="assets/img/Facebook/657dab9bde46484e80e2f5c11bd21b77.jpg" alt="" style="height:400px; border-top-left-radius: 3rem; border-bottom-right-radius: 3rem; box-shadow: 10px 12px 12px 12px rgba(192, 191, 191, 0.185);">
+            </div>
+            <div class="col-md-6" id="greens">
+              <h3><b>The best food comfort will <br> always be greens, corn bread,<br> and fried chicken. <b></h3>
+              <p><small>Lorem ipsum dolor sit, amet consectetur adipisicing elit. <br> Voluptatibus molestiae architecto amet ipsum <br> quas nostrum sed beatae quisquam sunt incidunt soluta. Id dolodi eos nemo.</small></p>
+              <a href=""><button type="button" id="buton" > Know More About Us</button></a>
+            </div>
+          </div>
+          <div class="row border">
+            <div class="col-md-6" id="greens">
+              <h3><b>The best food comfort will <br> always be greens, corn bread,<br> and fried chicken. <b></h3>
+              <p><small>Lorem ipsum dolor sit, amet consectetur adipisicing elit. <br> Voluptatibus molestiae architecto amet ipsum <br> quas nostrum sed beatae quisquam sunt incidunt soluta. Id dolodi eos nemo.</small></p>
+              <a href=""><button type="button" id="buton" > Know More About Us</button></a>
+            </div>
+            <div class="col-md-6 border">
+              <img src="assets/img/Facebook/657dab9bde46484e80e2f5c11bd21b77.jpg" alt="" style="height:400px; border-top-left-radius: 3rem; border-bottom-right-radius: 3rem; box-shadow: 10px 12px 12px 12px rgba(192, 191, 191, 0.185);">
+            </div>
+            <!-- <div class="col-sm-6 border">
+              <h3><b>The best food comfort will <br> always be greens, corn bread,<br> and fried chicken. <b></h3>
+                <p><small>Lorem ipsum dolor sit, amet consectetur adipisicing elit. <br> Voluptatibus molestiae architecto amet ipsum <br> quas nostrum sed beatae quisquam sunt incidunt soluta. Id dolodi eos nemo.</small></p>
+                
+            </div>
+            <div class="col-sm-6 border">
+              <h3><b>The best food comfort will <br> always be greens, corn bread,<br> and fried chicken. <b></h3>
+                <p><small>Lorem ipsum dolor sit, amet consectetur adipisicing elit. <br> Voluptatibus molestiae architecto amet ipsum <br> quas nostrum sed beatae quisquam sunt incidunt soluta. Id dolodi eos nemo.</small></p>
+                <a href=""><button type="button" id="buton" > Know More About Us</button></a>
+              <!-- <img src="assets/img/food3-4198040.jpeg" alt="" width="50%"> -->
+              <!-- <img src="assets/img/food3-4198040.jpeg" alt="" width="50%"> -->
+            </div> -->
+            <!-- <div class="col-md-6"  id="greens">
+              
+                <p><small>Lorem ipsum dolor sit, amet consectetur adipisicing elit. <br> Voluptatibus molestiae architecto amet ipsum <br> quas nostrum sed beatae quisquam sunt incidunt soluta. Id dolodi eos nemo.</small></p>
+                <a href=""><button type="button" id="buton" > Know More About Us</button></a>
+            </div>
+            <div class="col-md-6  mb-5 arts">
+              <div class="row no-gutters">
+                <div class="col-md-6">
+                  <div class="text-right leftimg">
+                    <img class="img-fluid" src="assets/img/Facebook/IMG-20200121-WA0007.jpg"  alt="">
+                  </div>
+                  <div class="mt-2">
+                    <img class="img-fluid" src="assets/img/Facebook/IMG-20200121-WA0007.jpg" alt="">
+                  </div>
+              </div>
+              <div class="col-md-6 rightimg">
+                <div>
+                  <img class="img-fluid" src="assets/img/Facebook/IMG-20200121-WA0007.jpg" alt="">
+                </div>
+                <div class="mt-2">
+                  <img class="img-fluid" src="assets/img/Facebook/IMG-20200121-WA0007.jpg" alt="">
+                </div>
+              </div>
+              </div> -->
+        </div>
+      </div>
+    </section>
+    <section class="container">
+      <div class="explore mt-5 text-center">
+        <h2><b>Explore our Foods</b></h2>
+        <p class="lore pt-4"><small>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br> Reiciendis ullam, maiores, repellat nesciunt quas harum commodi veniam no</small></p>
+      </div>
+      <!--card section-->
+            <!--card ends-->
+      <div class="uton text-center">
+        <a href=""><button type="button " id="uton" > Explore all food.</button></a>
+      </div> 
+    </section>
